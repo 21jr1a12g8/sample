274becab9bcaa000bf586072e2afe3a8fa4dba96
21jr1a12g8 <125033340+21jr1a12g8@users.noreply.github.com>
2025-03-29 12:35:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function clickme() {
+    let x = "You Clicked Me!";
+    document.getElementById("hello").innerHTML = x;
+    document.getElementById("hello").style.color = "#ff6347"; // Change color to tomato
+}
